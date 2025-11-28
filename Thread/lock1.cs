f290e163e9c52cbc45c2dbd040a9f6ab72db69ec
David Lancioni <dlancioni@gmail.com>
2025-11-28 13:56:46 -0300

    Implement counter increment with Monitor.Enter

--- Thread/lock1.cs
+++ Thread/lock1.cs
@@ -3,8 +3,9 @@ using System.Threading.Tasks;
 using System.Management;
 using System.Collections.Immutable;
 
+//
 // Locks automatically released when leaving the lock block or fail
-
+//
 public class Program
 {
     public static void Main()
@@ -37,7 +38,9 @@ public class Program
 }
 
 
-
+//
+// Using Interlocked
+//
 using System;
 using System.Threading; // Required for Thread and Interlocked
 
@@ -65,6 +68,53 @@ public class Program
                 // Atomically increments the variable
                 Interlocked.Increment(ref counter); 
             }
+        }    
+    }
+}
+
+
+//
+// Using Monitor.Enter (at the end locks are compiled into monitor)
+//
+using System;
+using System.Threading.Tasks;
+using System.Management;
+using System.Collections.Immutable;
+
+// Locks automatically released when leaving the lock block or fail
+
+public class Program
+{
+    public static void Main()
+    {
+        int counter = 0;
+        object counterLock = new object();
+
+        Thread t1 = new Thread(IncrementCounter);
+        Thread t2 = new Thread(IncrementCounter);        
+
+        t1.Start();        
+        t2.Start();        
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine($"Counter Value is: {counter}");
+
+        void IncrementCounter()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Monitor.Enter (counterLock);
+                try
+                {
+                    counter = counter + 1;
+                }
+                finally
+                {
+                    Monitor.Exit (counterLock);
+                }   
+            }    
         }    
     }
 }
