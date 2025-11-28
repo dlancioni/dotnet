68906843d91096cb8c551a42838268dbac18be37
David Lancioni <dlancioni@gmail.com>
2025-11-28 11:12:27 -0300

    Add multi-threaded counter with lock mechanism
    Implement a multi-threaded counter with locking.

---
+++ Thread/lock1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading.Tasks;
+using System.Management;
+using System.Collections.Immutable;
+
+// Locks automatically released when leaving the lock block or fail
+
+public class Program
+{
+    public static void Main()
+    {
+        int counter = 0;
+        System.Threading.Lock counterLock = new Lock();
+
+        Thread t1 = new Thread(IncrementCounter);
+        Thread t2 = new Thread(IncrementCounter);        
+
+        t1.Start();        
+        t2.Start();        
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine($"Counter Value is: {counter}");
+
+        void IncrementCounter()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                lock (counterLock)
+                {
+                    counter = counter + 1;
+                }
+            }    
+        }    
+    }
+}
